cc9595831086ad4faaf7a9835fae284b501f5e38
FH-1504 <roshini.p@foodhub.com>
2023-05-02 18:27:57 +0530

    spaces removed

--- src/test/java/com/apiautomation/Digitalboard.java
+++ src/test/java/com/apiautomation/Digitalboard.java
@@ -1,41 +1,30 @@
 package com.apiautomation;
-
 import com.github.wnameless.json.flattener.JsonFlattener;
 import io.restassured.http.ContentType;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
-
 import static io.restassured.RestAssured.*;
-
 public class Digitalboard {
     Object response = null;
-
     String resourceid = null;
     String tvtoken = null;
-
     String serialNumber=null;
-
     String tvName=null;
-
     Map<String, Object> fparams = new HashMap<>();
     Map<String, Object> qparams = new HashMap<>();
     JSONParser parser = new JSONParser();
     Object obj = parser.parse(new FileReader("testdata/testenv.json"));
     JSONObject jsonObject = (JSONObject)obj;
     Map<String, Object> stringObjectMap = JsonFlattener.flattenAsMap(((JSONObject) obj).toJSONString());
-
     String token = stringObjectMap.get("roshinifoodhubtest.token").toString();
     String siturl = stringObjectMap.get("api.url").toString();
-
-
     public Digitalboard() throws IOException, ParseException {
     }
     public static int getRandom(int var1, int var2) {
@@ -49,18 +38,14 @@ public class Digitalboard {
         int var5 = var4.nextInt(var2) + var1;
         System.out.println("number" + var5);
         return var5;
-
     }
-
 
     @Test(priority = 0)
     public void addtvlegacy() {
-
         try {
                 serialNumber = "ob-ca-cc" + getRandom(111,999);
                 tvName = "RoshiniFoodhub" + getRandom(111,999);
             // Get the body data which is in form-data in postman
-
                fparams.put("tv_name", tvName);
                 fparams.put("tv_serialnumber", serialNumber);
                 fparams.put("slide_duration", "10");
@@ -70,20 +55,13 @@ public class Digitalboard {
                 fparams.put("show_logo", "YES");
                 fparams.put("show_door_delivery", "YES");
                 fparams.put("type", "ORDER_VIEW_SCREEN");
-
                 //getting the data from json file
                 // file path is root directory path
                 // Once after bringing the json convert into map by using json flattener for key value which is written globally
-
-
                 // Get the body data which is in params in postman
                 qparams.put("api_token", token);
                 baseURI = ""+ siturl + "/television";
-                response = given().formParams(fparams).
-                        when().queryParams(qparams).
-                        post(baseURI).
-                        then().
-                        extract().response().asString();
+                response = given().formParams(fparams).when().queryParams(qparams).post(baseURI).then().extract().response().asString();
                 System.out.println(response);
                 Map<String, Object> map = JsonFlattener.flattenAsMap((response).toString());
                 System.out.println(map);
@@ -91,16 +69,10 @@ public class Digitalboard {
                 System.out.println(resourceid);
                 qparams.clear();
                 fparams.clear();
-
-
-
             } catch (Exception e) {
                 hardFail("Failed to add TV" + e);
             }
         }
-
-
-
     @Test(priority = 1)
         public void gettvtoken(){
         try {
@@ -121,32 +93,20 @@ public class Digitalboard {
     @Test(priority = 2)
     public void gettvboard() {
         try {
-
             qparams.put("api_token", tvtoken);
             baseURI = siturl + "/client/television/board";
-            response = given().queryParams(qparams).
-                    when().
-                    get(baseURI).
-                    then().extract().response().asPrettyString();
+            response = given().queryParams(qparams).when().get(baseURI).then().extract().response().asPrettyString();
             System.out.println("before is " + response);
             Map<String, Object> map = JsonFlattener.flattenAsMap((response).toString());
             for (Map.Entry<String,Object> rest: map.entrySet()){
                 System.out.println("Key : "+rest.getKey() +"  "+"Value : "+rest.getValue());
             }
             System.out.println(map);
-
         } catch (Exception e) {
             hardFail("Failed to getTV details" + e);
         }
     }
-
-
-
-
-
-        private void hardFail(String s) {
+    private void hardFail(String s) {
             System.out.println(s);
     }
-
-
 }
